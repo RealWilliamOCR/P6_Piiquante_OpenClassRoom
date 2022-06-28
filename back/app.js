673b92391bc8badd560b7c5bcc9cd4d7076d189d
RealWilliamOCR <william.ranc@visiativ.com>
2022-06-28 14:31:57 +0200

    Commit du projet 6 plus création des fichiers app.js et server.js

---
+++ back/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+});
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé !'
+    });
+});
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://WilliamTheBest:Cody_RhodesWrestleMania38@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
