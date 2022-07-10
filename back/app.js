09a8ebe250d861689f279eba1408be542e76bca2
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-07-10 12:16:16 +0200

    Commit
    Ajout de la route sauce dans app.js

--- back/app.js
+++ back/app.js
@@ -5,7 +5,7 @@ module.exports = app;
 
 const mongoose = require('mongoose');
 
-const stuffRoutes = require('./routes/stuff');
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://WillOCR:0000@hottakesp6.mdcp4qa.mongodb.net/?retryWrites=true&w=majority', {
@@ -22,9 +22,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
+  });
 
-app.use('/api/stuff', stuffRoutes);
+ app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
