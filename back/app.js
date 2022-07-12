17f56b0b5ef8c510abeb2de5c18ac43d6d28542d
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-07-12 15:59:23 +0200

    Commit

--- back/app.js
+++ back/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const helmet = require("helmet");
+const path = require('path');
 
 module.exports = app;
 
@@ -8,7 +10,7 @@ const mongoose = require('mongoose');
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb+srv://WillOCR:0000@hottakesp6.mdcp4qa.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://WillOCR:AZERTY@hottakesp6.mdcp4qa.mongodb.net/?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
@@ -22,9 +24,12 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
- app.use('/api/sauces', sauceRoutes);
+app.use(helmet());
+
+app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, '../images')));
 
 module.exports = app;
