63ac96fceaa71ae48a5ace7b9f4b062dcad5d719
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-07-12 16:46:06 +0200

    Commit

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -5,11 +5,14 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
-        if (req.body.userId && req.body.userId !== userId) {
+        /*if (req.body.userId && req.body.userId !== userId) {
             throw 'User ID non valable!';
         } else {
             next();
-        }
+        }*/
+        req.auth = {
+            userId: userId,
+        };
     } catch {
         res.status(401).json({
             error: new Error('Requête non authentifiée')
