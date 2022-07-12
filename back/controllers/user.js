bc6602ed58dc5b3686b18ee70fe3f9ed4293695a
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-07-12 15:57:42 +0200

    Commit

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,48 +1,42 @@
-const bcrypt= require('bcrypt');
-const jwk = require('jsonwebtoken');
-const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const User = require('../models/User.js');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        const user = new User({
-          email: req.body.email,
-          password: hash
-        });
-        user.save()
-          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          .catch(error => res.status(400).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
-  };
+    bcrypt.hash(req.body.password, 10) //hash le mot de passe, on execute 10 fois l'algorithme de hachage
+        .then(hash => {
+            const user = new User({
+                email: emailHash,
+                password: hash
+            });
+
+            user.save() //enregistrement de l utilisateur avec retour promise
+                .then(() => res.status(201).json({ message: 'New Utilisateur créé !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
 
 exports.login = (req, res, next) => {
-    User.findOne({email: req.body.email})
-    .then(user => {
-        if(user => null) {
-            res.status(401).json({message: 'Paire identifiant/mot de passe incorrecte'});
-        } else {
+    console.log('Connecté en tant que : ' + req.body.email);
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            }
             bcrypt.compare(req.body.password, user.password)
-            .then (valid => {
-                if (!valid) {
-                    res.status(401).json({message: 'Paire identifiant/mot de passe incorrecte'});
-                } else {
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                    }
                     res.status(200).json({
                         userId: user._id,
-                        token: jwt.sign(
-                            {userId: user._Id},
-                            'RANDOM_TOKEN_SECRET',
-                            {expiresIn: '24h'}
+                        token: jwt.sign({ userId: user._id },
+                            'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }
                         )
                     });
-                }
-            })
-            .catch(error => {
-                res.status(500).json({error});
-            })
-        }
-    })
-    .catch(error => {
-        res.status(500).json({error});
-    })
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
