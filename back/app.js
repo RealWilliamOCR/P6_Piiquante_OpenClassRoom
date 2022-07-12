95f5279741d299a2fbbdd925c11f16d708ded7ad
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-07-12 16:46:32 +0200

    Commit

--- back/app.js
+++ back/app.js
@@ -30,6 +30,6 @@ app.use(helmet());
 
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
-app.use('/images', express.static(path.join(__dirname, '../images')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
