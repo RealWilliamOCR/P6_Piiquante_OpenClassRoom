4ef6bbb47fe368c593fb1e4dd9778d1780e9da92
RealWilliamOCR <82954573+RealWilliamOCR@users.noreply.github.com>
2022-07-14 11:45:54 +0200

    Commit
    Modification et suppression d'une sauce fonctionnel.

--- back/routes/sauce.js
+++ back/routes/sauce.js
@@ -7,6 +7,8 @@ const multer = require('../middleware/multer-config');
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.delete('/:id', auth, multer, sauceCtrl.deleteSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 
 
 module.exports = router;
